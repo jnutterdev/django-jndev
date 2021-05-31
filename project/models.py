096880e38a39379d630d593a34f90def27fe5270
jnutterdev <hello@jnutterdev.com>
2021-05-31 11:44:49 -0400

    blog entries can now accept an image for upload, displays thumbnail on home page and full image on post_detail page

--- project/models.py
+++ project/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
     body = models.TextField()
-    # image = look into adding an image field
+    cover = models.ImageField(upload_to='images/', null=True)
     # do I need a date field for each post? 
 
     def __str__(self):
