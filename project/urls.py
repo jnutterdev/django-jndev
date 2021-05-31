288ddc3f7335ac2aaac25d526d93e3345daf0402
jnutterdev <hello@jnutterdev.com>
2021-05-31 09:45:38 -0400

    Blog now takes a blog post, renders post on home page and in a detailed post page

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProjectListView
+from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
-    path('', ProjectListView.as_view(), name='home')
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('', BlogListView.as_view(), name='home'),
 ]
--- project/views.py
+++ project/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
+
+from .models import Post
 # Create your views here.
 
-class ProjectListView(TemplateView):
-    template_name = 'base.html'
+class BlogListView(ListView):
+    model = Post
+    template_name = 'layouts/home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'layouts/post_detail.html'
