994e2e082c40f7ef64820e80e16740703b8de39a
jnutterdev <hello@jnutterdev.com>
2021-05-31 21:21:34 -0400

    switched db over to psql

--- project_files/settings.py
+++ project_files/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'project_files.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DJANGO_DBNAME"),
+        'USER': os.getenv("DJANGO_DBUSER"),
+        'PASSWORD': os.getenv("DJANGO_DBPWD"),
+        'HOST': 'localhost', 
+        'PORT': 5432
     }
 }
 
