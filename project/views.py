da9ff995def5e2b1e0e768c189dfc95e299ae6fd
jnutterdev <hello@jnutterdev.com>
2021-06-26 16:58:48 -0400

    work toward newer version of the site, trying to move away from using a template

--- project/views.py
+++ project/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import TemplateView
 
 from .models import Post
 # Create your views here.
 
-class BlogListView(ListView):
-    model = Post
+class HomePageView(TemplateView):
     template_name = 'layouts/home.html'
 
-class BlogDetailView(DetailView):
-    model = Post
-    template_name = 'layouts/post_detail.html'
+class AboutPageView(TemplateView):
+    template_name = 'layouts/about.html'
+
